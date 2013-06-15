ff2e6ad3b1113f317cb4a10b8f688c63e040143a
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-06-15 13:51:14 +0200

    Print error if I2C fails

--- Firmware/Balanduino/I2C.ino
+++ Firmware/Balanduino/I2C.ino
@@ -9,15 +9,24 @@ uint8_t i2cWrite(uint8_t registerAddress, uint8_t* data, uint8_t length, bool se
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
   Wire.write(data, length);
-  return Wire.endTransmission(sendStop); // Returns 0 on success
+  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+  if(rcode) {
+    Serial.print(F("i2cWrite failed: "));
+    Serial.println(rcode);
+  }
+  return rcode;
 }
 
 uint8_t i2cRead(uint8_t registerAddress, uint8_t* data, uint8_t nbytes) {
   uint32_t timeOutTimer;
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
-  if(Wire.endTransmission(false)) // Don't release the bus
-    return 1; // Error in communication
+  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+  if(rcode) {
+    Serial.print(F("i2cRead failed: "));
+    Serial.println(rcode);
+    return rcode;
+  }
   Wire.requestFrom(IMUAddress, nbytes,(uint8_t)true); // Send a repeated start and then release the bus after reading
   for(uint8_t i = 0; i < nbytes; i++) {
     if(Wire.available())
@@ -27,8 +36,10 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t* data, uint8_t nbytes) {
       while(((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
       if(Wire.available())
         data[i] = Wire.read();
-      else
-        return 2; // Error in communication
+      else {
+        Serial.println(F("i2cRead timeout"));
+        return 5; // This error value is not already taken by endTransmission
+      }
     }
   }
   return 0; // Success
