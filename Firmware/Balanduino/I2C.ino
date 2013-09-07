4bb7b094c9c76ba87db30324d2541832397aee67
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-09-08 00:08:46 +0200

    Decreased I2C_TIMEOUT

--- Firmware/Balanduino/I2C.ino
+++ Firmware/Balanduino/I2C.ino
@@ -1,5 +1,5 @@
 const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
-const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
+const uint16_t I2C_TIMEOUT = 100; // Used to check for errors in I2C communication
 
 uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
   return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
