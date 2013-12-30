88ce8b5c7fb29d765c1ae5c46186cda611bdc7f8
Kristian Lauszus <lauszus@gmail.com>
2013-12-30 22:29:27 +0100

    Optimized driving forward/backward and left/right code

--- Firmware/Balanduino/Motor.ino
+++ Firmware/Balanduino/Motor.ino
@@ -1,17 +1,6 @@
 void updatePID(double restAngle, double offset, double turning, double dt) {
-  /* Steer robot */
-  if (offset > 0) { // Forward
-    if (wheelVelocity < 0)
-      offset += (double)wheelVelocity / velocityScaleMove; // Scale down offset at high speed - wheel velocity is negative when driving forward
-    restAngle -= offset;
-  }
-  else if (offset < 0) { // Backward
-    if (wheelVelocity > 0)
-      offset += (double)wheelVelocity / velocityScaleMove; // Scale down offset at high speed - wheel velocity is positive when driving backward
-    restAngle -= offset;
-  }
   /* Brake */
-  else if (steerStop) {
+  if (steerStop) {
     int32_t wheelPosition = getWheelsPosition();
     int32_t positionError = wheelPosition - targetPosition;
     if (cfg.backToSpot) {
@@ -31,10 +20,13 @@ void updatePID(double restAngle, double offset, double turning, double dt) {
     }
     restAngle -= (double)wheelVelocity / velocityScaleStop;
 
-    if (restAngle < cfg.targetAngle - 10) // Limit rest Angle
-      restAngle = cfg.targetAngle - 10;
-    else if (restAngle > cfg.targetAngle + 10)
-      restAngle = cfg.targetAngle + 10;
+    restAngle = constrain(restAngle, cfg.targetAngle - 10, cfg.targetAngle + 10); // Limit rest Angle
+  }
+  /* Drive forward and backward */
+  else {
+    if ((offset > 0 && wheelVelocity < 0) || (offset < 0 && wheelVelocity > 0) || offset == 0) // Scale down offset at high speed - wheel velocity is negative when driving forward and positive when driving backward
+      offset += (double)wheelVelocity / velocityScaleMove; // We will always compensate if the offset is 0, but steerStop is not set
+    restAngle -= offset;
   }
 
   if (restAngle - lastRestAngle > 1) // Don't change restAngle with more than 1 degree in each loop
@@ -47,8 +39,7 @@ void updatePID(double restAngle, double offset, double turning, double dt) {
   error = (restAngle - pitch);
   pTerm = cfg.P * error;
   integratedError += error * dt;
-  integratedError = constrain(integratedError, -1.0, 1.0); // Limit the integrated error
-  iTerm = (cfg.I * 100.0) * integratedError;
+  iTerm = (cfg.I * 100.0) * constrain(integratedError, -1.0, 1.0); // Limit the integrated error
   dTerm = (cfg.D / 100.0) * (error - lastError) / dt;
   lastError = error;
   PIDValue = pTerm + iTerm + dTerm;
@@ -58,20 +49,15 @@ void updatePID(double restAngle, double offset, double turning, double dt) {
     turning += abs((double)wheelVelocity / velocityScaleTurning); // Scale down at high speed
     if (turning > 0)
       turning = 0;
-    PIDLeft = PIDValue + turning;
-    PIDRight = PIDValue - turning;
   }
   else if (turning > 0) { // Right
     turning -= abs((double)wheelVelocity / velocityScaleTurning); // Scale down at high speed
     if (turning < 0)
       turning = 0;
-    PIDLeft = PIDValue + turning;
-    PIDRight = PIDValue - turning;
   }
-  else {
-    PIDLeft = PIDValue;
-    PIDRight = PIDValue;
-  }
+
+  PIDLeft = PIDValue + turning;
+  PIDRight = PIDValue - turning;
 
   PIDLeft *= cfg.leftMotorScaler; // Compensate for difference in some of the motors
   PIDRight *= cfg.rightMotorScaler;
@@ -90,24 +76,23 @@ void updatePID(double restAngle, double offset, double turning, double dt) {
 void moveMotor(Command motor, Command direction, double speedRaw) { // Speed is a value in percentage 0-100%
   if (speedRaw > 100)
     speedRaw = 100.0;
-  uint16_t speed = speedRaw * ((double)PWMVALUE) / 100.0; // Scale from 0-100 to 0-PWMVALUE
-  setPWM(motor, speed); // Set PWM value
+  setPWM(motor, speedRaw * ((double)PWMVALUE) / 100.0); // Scale from 0-100 to 0-PWMVALUE
   if (motor == left) {
     if (direction == forward) {
       leftA::Clear();
       leftB::Set();
     }
-    else if (direction == backward) {
+    else {
       leftA::Set();
       leftB::Clear();
     }
   }
-  else if (motor == right) {
+  else {
     if (direction == forward) {
       rightA::Set();
       rightB::Clear();
     }
-    else if (direction == backward) {
+    else {
       rightA::Clear();
       rightB::Set();
     }
@@ -120,7 +105,7 @@ void stopMotor(Command motor) {
     leftA::Set();
     leftB::Set();
   }
-  else if (motor == right) {
+  else {
     rightA::Set();
     rightB::Set();
   }
@@ -129,7 +114,7 @@ void stopMotor(Command motor) {
 void setPWM(Command motor, uint16_t dutyCycle) { // dutyCycle is a value between 0-ICR1
   if (motor == left)
     OCR1A = dutyCycle;
-  else if (motor == right)
+  else
     OCR1B = dutyCycle;
 }
 
